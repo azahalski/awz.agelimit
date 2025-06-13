5b2cbbc889c0ada01eeb74d1b9277cefd853d58b
Andrew Zahalski <andrew@zahalski.dev>
2025-06-13 20:52:55 +0300

    gulp checkpoint

---
+++ update/1.1.2/lang/ru/install/index.php
@@ -0,0 +1,9 @@
+<?php
+$MESS['AWZ_AGELIMIT_MODULE_NAME'] = 'AWZ: Возрастное ограничение';
+$MESS['AWZ_AGELIMIT_MODULE_DESCRIPTION'] = 'Модуль для вывода окна подтверждения совершеннолетия';
+$MESS["AWZ_PARTNER_NAME"] = "Andrew Zahalski";
+$MESS["AWZ_PARTNER_URI"] = "https://zahalski.dev";
+$MESS["AWZ_AGELIMIT_INSTALL_TITLE"] = "Удаление модуля AWZ: Возрастное ограничение";
+$MESS['MOD_UNINST_SAVE_OPTS'] = "Вы можете сохранить основные настройки модуля";
+$MESS['MOD_UNINST_SAVE_EVENTS_DESC'] = "Вы можете сохранить почтовые шаблоны";
+$MESS['MOD_UNINST_SAVE_OPTS_LABEL'] = "Сохранить настройки";
